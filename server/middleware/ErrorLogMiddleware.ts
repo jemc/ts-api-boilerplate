f291d425521bb838c8bcaea67be7817fe146dce3
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2020-10-19 13:36:49 -0700

    Fix ErrorLogMiddleware parameter signature.

--- server/middleware/ErrorLogMiddleware.ts
+++ server/middleware/ErrorLogMiddleware.ts
@@ -1,10 +1,15 @@
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from "express"
 
 export default function ErrorLogMiddleware(
   err: Error,
   req: Request,
   res: Response,
+  // We need to include the next function or express will call it automatically.
+  // We *don't* want it to be called, because it would show the stack trace
+  // to the end user, which is a bad practice and security issue.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
 ) {
-  console.log(err)
+  console.log("Uncaught error:", err)
   res.sendStatus(500)
 }
